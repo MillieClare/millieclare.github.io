e2b9831cd9d6eccda96c7dcaf220b84f56af493c
MillieClare <40922831+MillieClare@users.noreply.github.com>
2018-07-25 11:00:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script>
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -8,4 +8,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
-</script>
