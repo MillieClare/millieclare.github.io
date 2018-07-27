08e6c895dd5d7502b7454c1ccfce070cc27e418b
MillieClare <40922831+MillieClare@users.noreply.github.com>
2018-07-27 15:09:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,59 +11,9 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
 }
 
-// Type carousel
-var TxtRotate = function(el, toRotate, period) {
-      this.toRotate = toRotate;
-      this.el = el;
-      this.loopNum = 0;
-      this.period = parseInt(period, 10) || 2000;
-      this.txt = '';
-      this.tick();
-      this.isDeleting = false;
-    };
 
-    TxtRotate.prototype.tick = function() {
-
-      var i = this.loopNum % this.toRotate.length;
-      var fullTxt = this.toRotate[i];
-
-      if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-
-      this.el.value =this.txt;
-
-      var that = this;
-      var delta = 300 - Math.random() * 100;
-
-      if (this.isDeleting) { delta /= 2; }
-
-      if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-      } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-      }
-
-      setTimeout(function() {
-        that.tick();
-      }, delta);
-    };
-
-    window.onload = function() {
-        var element = document.getElementById('myInput');
-        var toRotate = element.getAttribute('data-rotate');
-        var period = element.getAttribute('data-period');
-         new TxtRotate(element, JSON.parse(toRotate), period);
-
-      }
-    
     //Loading bar
-;function(){
+function(){
  
     var minloadingtime = 100
     var maxloadingtime = 3000
@@ -92,7 +42,7 @@ var TxtRotate = function(el, toRotate, period) {
 
 //in case browser does not support transition
 
-;(function(){ // Final page transition code
+(function(){ // Final page transition code
  
     var animation = false,
     animationstring = 'animation',
@@ -156,3 +106,56 @@ var TxtRotate = function(el, toRotate, period) {
     }
  
 })();
+
+
+// Type carousel
+var TxtRotate = function(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function() {
+
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.value =this.txt;
+
+      var that = this;
+      var delta = 300 - Math.random() * 100;
+
+      if (this.isDeleting) { delta /= 2; }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function() {
+        that.tick();
+      }, delta);
+    };
+
+    window.onload = function() {
+        var element = document.getElementById('myInput');
+        var toRotate = element.getAttribute('data-rotate');
+        var period = element.getAttribute('data-period');
+         new TxtRotate(element, JSON.parse(toRotate), period);
+
+      }
+    
